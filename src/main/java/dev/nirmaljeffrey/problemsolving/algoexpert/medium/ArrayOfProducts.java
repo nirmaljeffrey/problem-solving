d2a1ecd99f6a6654d70e12f4788bda9b95094290
Nirmal Jeffrey <jeffattry@gmail.com>
2021-08-01 19:19:11 +0530

    feat: day 6 algoexpert problem 1

---
+++ src/main/java/dev/nirmaljeffrey/problemsolving/algoexpert/medium/ArrayOfProducts.java
@@ -0,0 +1,30 @@
+package dev.nirmaljeffrey.problemsolving.algoexpert.medium;
+
+import java.util.Arrays;
+
+public class ArrayOfProducts {
+    // O(N) time | O(N) space
+    public static int[] arrayOfProducts(int[] array) {
+        // Write your code here.
+        int[] outputArray = new int[array.length];
+        int product = 1;
+
+        for (int i = 0; i < array.length; i++) {
+            outputArray[i] = product;
+            product *= array[i];
+        }
+
+        product = 1;
+
+        for (int i = array.length - 1; i >= 0; i--) {
+            outputArray[i] *= product;
+            product*= array[i];
+        }
+        return outputArray;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {5, 1, 4, 2};
+        System.out.println(Arrays.toString(arrayOfProducts(array)));
+    }
+}
